fba88e57992f8a6b0ebf43744997829280c4969a
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2022-12-14 00:02:59 +0100

    App.java now contains only those lines of code that are responsible for the main menu of the Brain games.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,23 +3,18 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class App {
+    public static String playerInput;
     public static void main(String[] args) {
         Scanner initialScanner = new Scanner(System.in);
 
-        System.out.println("Please enter the game number and press Enter. \n 0 - Exit  \n 1 - Greet \n 2 - Even");
-        var playerInput = initialScanner.next();
-
-        if (playerInput.equals("1")) {
-            System.out.println("Your choice: 1");
-            Cli.makeTheAcquaintance();
-        } else if (playerInput.equals("0")) {
-            System.out.println("Your choice: 0");
-        } else if (playerInput.equals("2")) {
-            System.out.println("Your choice: 2");
-            Even.isEvenGreeting();
-        } else {
-            System.out.println("Wrong input");
-        }
+        System.out.println("Please enter the game number and press Enter. "
+                + "\n 0 - Exit"
+                + "\n 1 - Greet"
+                + "\n 2 - Even"
+                + "\n 3 - Calc");
+        playerInput = initialScanner.next();
+        System.out.println("Your choice is " + playerInput);
+        Engine.startEngine();
         initialScanner.close();
     }
 }
