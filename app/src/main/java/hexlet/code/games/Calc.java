63b82ab50d037ee5678013adf4bbfd53777ee505
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2022-12-14 00:00:56 +0100

    Created a new game - Calculator. It shows random expression (adding, subtraction or multiplying) that the player should resolve. For victory three expressions have to be resolved.

---
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,56 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+import static hexlet.code.Engine.playerName;
+
+public class Calc {
+    public static void startCalcGame() {
+        System.out.println("What is the result of the expression?");
+        var victoryCount = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        for (var i = 0; i <= 4; i++) {
+            // Initializing two different numbers as operands for our expressions
+            int operand1 = (int) (0 + Math.random() * 100);
+            int operand2 = (int) (0 + Math.random() * 100);
+
+            // Create an array with the result of the different expressions (with different operators).
+            int[] resultsOfExpressions = new int[3];
+            resultsOfExpressions[0] = operand1 + operand2;
+            resultsOfExpressions[1] = operand1 - operand2;
+            resultsOfExpressions[2] = operand1 * operand2;
+
+            /* Create an array with the same expressions as given above but as Strings.
+            It is used to show expressions that should be resolved to the player.*/
+            String[] expressionsAsStrings = new String[resultsOfExpressions.length];
+            expressionsAsStrings[0] = operand1 + " + " + operand2;
+            expressionsAsStrings[1] = operand1 + " - " + operand2;
+            expressionsAsStrings[2] = operand1 + " * " + operand2;
+
+            /* Create a random index within the bounds of the length of our arrays.
+            It will be used to randomly choose an expression result and expression results as a String*/
+            int bound = resultsOfExpressions.length;
+            int randomIndex = (int) (0 + Math.random() * bound);
+
+            /* Define what expression will be displayed and should be resolved by the player.
+            Plus define a variable that will be use to check the answer*/
+            int expressionResult = resultsOfExpressions[randomIndex];
+            System.out.println(expressionsAsStrings[randomIndex]);
+
+            var playerAnswer = scanner.next();
+            if (expressionResult == Integer.parseInt(playerAnswer)) {
+                System.out.println("Your answer is: " + playerAnswer + "\nCorrect!");
+                i++;
+                victoryCount += 1;
+                if (victoryCount == 3) {
+                    System.out.println("Congratulations, " + playerName + "!");
+                    break;
+                }
+            } else {
+                System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was " + expressionResult
+                        + "\nLet's try again, " + playerName);
+                break;
+            }
+        }
+    }
+}
