ca7434b1d1cdccd2abac7e4a652981a7970ff391
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2022-12-13 23:56:59 +0100

    Removed lines that were responsible for a player's name and greeting. They were moved to Engine.java.

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,19 +1,11 @@
-package hexlet.code;
+package hexlet.code.games;
 
 import java.util.Scanner;
+import static hexlet.code.Engine.playerName;
 
 public class Even {
-    private static String playerName;
-    public static void isEvenGreeting() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        playerName = scanner.next();
-        System.out.println("Hello, " + playerName + "!");
+    public static void startEvenGame() {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        isEvenGame();
-        }
-    public static void isEvenGame() {
         var victoryCount = 0;
         Scanner scanner = new Scanner(System.in);
         for (var i = 0; i <= 4; i++) {
@@ -32,10 +24,10 @@ public class Even {
                 System.out.println("Correct!");
                 i++;
                 victoryCount += 1;
-                    if (victoryCount == 3) {
-                        System.out.println("Congratulations, " + playerName + "!");
-                        break;
-                    }
+                if (victoryCount == 3) {
+                    System.out.println("Congratulations, " + playerName + "!");
+                    break;
+                }
             } else if (number % 2 == 0 && playerAnswer.equals("no")) {
                 System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.");
                 break;
