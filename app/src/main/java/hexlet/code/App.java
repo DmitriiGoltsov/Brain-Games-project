6af0099c0097cff5a96faa6246f0d67d0edc3419
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2022-12-17 23:15:05 +0100

    Added the Prime game to the menu. Also changed the position of the exit number in the menu: now it is the last option.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,14 +8,16 @@ public class App {
         Scanner initialScanner = new Scanner(System.in);
 
         System.out.println("Please enter the game number and press Enter. "
-                + "\n 0 - Exit"
                 + "\n 1 - Greet"
                 + "\n 2 - Even"
                 + "\n 3 - Calc"
                 + "\n 4 - GCD"
-                + "\n 5 - Progression");
+                + "\n 5 - Progression"
+                + "\n 6 - Prime"
+                + "\n 0 - Exit");
         playerInput = initialScanner.next();
         System.out.println("Your choice is " + playerInput);
         Engine.startEngine();
+        initialScanner.close();
     }
 }
