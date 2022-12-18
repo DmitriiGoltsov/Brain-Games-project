edca0796c375025048c7240a40c4231eb473eebb
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2022-12-18 23:34:15 +0100

    Tried to improved maintainability of the code. Got rid of same useless lines.

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,8 +5,7 @@ public class Cli {
     public static void makeTheAcquaintance() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
+        System.out.println("Welcome to the Brain Games!" + "\nMay I have your name?");
         String playerName = scanner.next();
         System.out.println("Hello, " + playerName + "!");
 
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -16,17 +16,9 @@ public class Engine {
     public static String getPlayerName() {
         return playerName;
     }
-    public static String startEngine() {
-        if (getPlayerInput().equals("0")) {
-            return null;
-        }
-        if (getPlayerInput().equals("1")) {
-            Cli.makeTheAcquaintance();
-            return null;
-        }
+    public static void startEngine() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nWelcome to the Brain Games!");
-        System.out.println("May I have your name?");
+        System.out.println("\nWelcome to the Brain Games!" + "\nMay I have your name?");
         playerName = scanner.next();
         System.out.println("\nHello, " + playerName + "!");
         if (getPlayerInput().equals("2")) {
@@ -39,10 +31,6 @@ public class Engine {
             Progression.startProgressionGame();
         } else if (getPlayerInput().equals("6")) {
             Prime.startPrimeGame();
-        } else {
-            System.out.println("Wrong input");
-            return null;
         }
-        return null;
     }
 }
