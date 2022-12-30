0c11f4b5be6b65b73da7be7c994f18742f76b4e1
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2022-12-30 17:12:02 +0100

    Game logic that is common with the logic of the other games was transferred to Engine class. isEven method was simplified and was made more abstract.

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -2,46 +2,31 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import hexlet.code.Utils;
-import java.util.Scanner;
 
 public class Even {
     public static void startEvenGame() {
-        Engine.askName();
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        String description = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+        final var numberOfRounds = 3;
+        String[] data = new String[numberOfRounds];
+        String[] correctAnswers = new String[numberOfRounds];
 
-        final var numberOfRounds = 3; // Player has to win 3 times to achieve the complete victory.
-        var victoryCount = 0;
-
-        for (var i = 0; i <= numberOfRounds; i++) {
-            Scanner scanner = new Scanner(System.in);
-            final int startOfRange = 0;
-            final int endOfRange = 1000;
-            int numberForGame = generateRoundData(startOfRange, endOfRange);
-
-            System.out.println("Question: " + numberForGame);
-            var playerAnswer = scanner.next();
-            if (isEven(numberForGame, playerAnswer)) {
-                System.out.println("Your answer: " + playerAnswer + "\nCorrect!");
-                victoryCount += 1;
-                if (victoryCount == numberOfRounds) {
-                    System.out.println(Engine.congratulate());
-                    break;
-                }
+        for (var i = 0; i < numberOfRounds; i++) {
+            int numberForGame = generateRoundData();
+            data[i] = Integer.toString(numberForGame);
+            if (isEven(numberForGame)) {
+                correctAnswers[i] = "yes";
             } else {
-                System.out.println(Engine.sendSadMessage(playerAnswer));
-                break;
+                correctAnswers[i] = "no";
             }
         }
+        Engine.run(description, data, correctAnswers);
     }
-    public static int generateRoundData(int lowRange, int highRange) {
-        return Utils.generateRanNum(lowRange, highRange);
+    public static int generateRoundData() {
+        final int startOfRange = 0;
+        final int endOfRange = 1000;
+        return Utils.generateRandomNumber(startOfRange, endOfRange);
     }
-
-    public static boolean isEven(int checkedNum, String checkedAnswer) {
-        if (checkedNum % 2 == 0 && checkedAnswer.equals("yes")) {
-            return true;
-        } else {
-            return checkedNum % 2 != 0 && checkedAnswer.equals("no");
-        }
+    public static boolean isEven(int checkedNum) {
+        return checkedNum % 2 == 0;
     }
 }
