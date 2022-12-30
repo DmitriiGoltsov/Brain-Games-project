cd6c2d6800b82dfc41b30c3db4a2ee3d628bf2cc
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2022-12-30 17:13:21 +0100

    Game logic that is common with the logic of the other games was transferred to Engine class. Unused method isGCD was removed.

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -2,57 +2,37 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 import hexlet.code.Utils;
-import java.util.Scanner;
 
 public class GCD {
-
     public static void startGCDGame() {
-        Engine.askName();
-        System.out.println("Find the greatest common divisor of given numbers.");
-        var victoryCount = 0;
-        Scanner scanner = new Scanner(System.in);
+        String description = "Find the greatest common divisor of given numbers.";
         final var numberOfRounds = 3;
+        String[] data = new String[numberOfRounds];
+        int[] correctAnswers = new int[numberOfRounds];
+
+        for (var i = 0; i < numberOfRounds; i++) {
+            int operand1 = generateRoundData();
+            String operand1AsString = Integer.toString(operand1);
+            int operand2 = generateRoundData();
+            String operand2AsString = Integer.toString(operand2);
+            int gCD = findGCD(operand1, operand2);
+            correctAnswers[i] = gCD;
+            data[i] =  operand1AsString + " " + operand2AsString;
+        }
+        Engine.run(description, data, correctAnswers);
+    }
+    public static int generateRoundData() {
         final int highRange = 100;
         final int lowRange = 1;
-
-        for (var i = 0; i <= numberOfRounds; i++) {
-            // Initializing two given numbers whose GCD should be found.
-            int operand1 = generateRoundData(lowRange, highRange);
-            int operand2 = generateRoundData(lowRange, highRange);
-            System.out.println("Question: " + operand1 + " " + operand2);
-
-            int gCD = findGCD(operand1, operand2);
-            int playerAnswer = Integer.parseInt(scanner.next());
-            if (isGCD(gCD, playerAnswer)) {
-                System.out.println("Your answer is: " + playerAnswer + "\nCorrect!");
-                victoryCount += 1;
-                if (victoryCount == numberOfRounds) {
-                    System.out.println(Engine.congratulate());
-                    break;
-                }
-            } else {
-                System.out.println(Engine.sendSadMessage(playerAnswer, gCD));
-                break;
-            }
-        }
+        return Utils.generateRandomNumber(lowRange, highRange);
     }
-
-    public static int generateRoundData(int lowRange, int highRange) {
-        return Utils.generateRanNum(lowRange, highRange);
-    }
-
     public static int findGCD(int operand1, int operand2) {
         int length = Math.max(operand1, operand2);
-
         int gCD = 1;
         for (var i2 = 1; i2 <= length; i2++) {
             if (operand1 % i2 == 0 && operand2 % i2 == 0) {
                 gCD = i2;
             }
         } return gCD;
-    }
-
-    public static boolean isGCD(int gCD, int playerAnswer) {
-        return gCD == playerAnswer;
     }
 }
