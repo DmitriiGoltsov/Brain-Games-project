5caf8c65aca301b2d0f3cc250c28c76ec6b70496
Dmitrii Goltsov <dmitriy.goltsov@mail.ru>
2023-01-11 22:08:19 +0100

    Added a whitespace before class declaration and makeTheAcquaintance method declaration.

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,8 +1,10 @@
 package hexlet.code;
 
 import java.util.Scanner;
+
 public class Cli {
     public static void makeTheAcquaintance() {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!" + "\nMay I have your name?");
